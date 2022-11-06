b79862e934b5bec8bdc2bdd383659aa8d8f79d63
Defake <defake@yandex.ru>
2022-11-06 01:50:56 +0100

    Add example of saving a task to db

--- domain/tasks/tasks.go
+++ domain/tasks/tasks.go
@@ -1,17 +1,40 @@
 package tasks
 
 import (
-	// "data// base/sql"
 	// "fmt"
-	// "github.com/Defake/day-assistant/data_access/db"
-	// "log"
+	sql "github.com/Defake/day-assistant/data_access/sql"
+	"log"
+	"encoding/json"
+	// "time"
 )
 
+// https://gobyexample.com/structs
 
+type Task struct {
+	id         uint64
+  Name       string `json:"name"`
+	IsProject  bool `json:"isProject"`
+	// weekDays   uint8 `json:"weekDays"`
+	// time       time.Time `json:"time"`
+}	
+
+func (t *Task) ToJsonString() (string, error) {
+	bytes, err := json.Marshal(t)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
 
 func SaveTask() {
 	// TODO
 
+	t := &Task{id: 1234, Name: "Test task", IsProject: true}
+	err := sql.UpsertRecord("tasks", t.id, t)
+	log.Printf("Upsert error: %s", err)
+	
+	// db.Connection.Query("")
 	// email := "admin@test.com"
 	// rows, err := db.Connection.Query("SELECT body->>'name' FROM users WHERE body->>'email' = $1", email)
 	// if err != nil {
